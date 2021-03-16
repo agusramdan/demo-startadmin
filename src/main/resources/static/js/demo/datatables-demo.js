74da73a00746cc97722c7dfb2aa6049c890f1a56
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2021-03-16 22:26:06 +0700

    commit add dataTables download button

--- src/main/resources/static/js/demo/datatables-demo.js
+++ src/main/resources/static/js/demo/datatables-demo.js
@@ -1,4 +1,12 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+//  $('#dataTable').DataTable();
+
+  var table = $('#dataTable').DataTable( {
+          buttons: [ 'copy', 'excel', 'pdf', 'print' ],
+          lengthChange: false
+      } );
+
+  table.buttons().container()
+      .appendTo( '#dataTable_wrapper .col-md-6:eq(0) ' );
 });
